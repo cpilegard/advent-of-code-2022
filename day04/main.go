74b90e3c62555e18c1e833d8b6f3c2cf73bed0e5
Cole Pilegard <cole.pilegard@stitchfix.com>
2022-12-04 09:54:24 -0800

    Add day 4 part 2

--- day04/main.go
+++ day04/main.go
@@ -10,9 +10,10 @@ import (
 
 func main() {
 	p1Result := P1(utils.ParseInput(os.Args[2]))
-	// p2Result := P2(utils.ParseInput(os.Args[2]))
+	p2Result := P2(utils.ParseInput(os.Args[2]))
 
 	fmt.Println(p1Result)
+	fmt.Println(p2Result)
 }
 
 func P1(input []string) int {
@@ -34,4 +35,29 @@ func P1(input []string) int {
 	}
 
 	return containingPairCount
+}
+
+// Establish the first pair as a boundary and check if the second pair overlaps
+func P2(input []string) int {
+	totalOverlapCount := 0
+
+	for _, pair := range input {
+		ranges := strings.Split(pair, ",")
+		range1 := ranges[0]
+		range2 := ranges[1]
+
+		boundsLow, _ := strconv.Atoi(strings.Split(range1, "-")[0])
+		boundsHigh, _ := strconv.Atoi(strings.Split(range1, "-")[1])
+		elfLow, _ := strconv.Atoi(strings.Split(range2, "-")[0])
+		elfHigh, _ := strconv.Atoi(strings.Split(range2, "-")[1])
+
+		if (elfLow >= boundsLow && elfLow <= boundsHigh) || // low is in bounds
+			(elfHigh <= boundsHigh && elfHigh >= boundsLow) || // high is in bounds
+			(elfLow < boundsLow && elfHigh > boundsHigh) { // bounds are encapsulated
+
+			totalOverlapCount++
+		}
+	}
+
+	return totalOverlapCount
 }
--- day04/main_test.go
+++ day04/main_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 )
 
@@ -19,9 +18,27 @@ func TestP1(t *testing.T) {
 	}
 
 	expected := 4
-
 	result := P1(input)
+
 	if result != expected {
-		fmt.Errorf("result: %d did not equal expected: %d", result, expected)
+		t.Errorf("result: %d did not equal expected: %d", result, expected)
+	}
+}
+
+func TestP2(t *testing.T) {
+	input := []string{
+		"2-4,6-8",
+		"2-3,4-5",
+		"6-6,4-6",  // yes, single overlap
+		"8-10,7-9", // yes, high overlap
+		"2-6,4-8",  // yes, low overlap
+		"2-8,3-7",  // yes, bounds encapsulated
+	}
+
+	expected := 4
+	result := P2(input)
+
+	if result != expected {
+		t.Errorf("result: %d did not equal expected: %d", result, expected)
 	}
 }
